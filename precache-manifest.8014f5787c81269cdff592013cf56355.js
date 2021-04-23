707d12c5f61857dcb32e2167a542208964052497
MatildaBjorken <matilda.bjork@hyperisland.se>
2021-04-23 11:00:03 +0200

    Updates

---
+++ precache-manifest.8014f5787c81269cdff592013cf56355.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9c7474061cd0ed1a67a5cb2840b48d",
+    "url": "/business-card/index.html"
+  },
+  {
+    "revision": "4b4406dd156f31b0805f",
+    "url": "/business-card/static/css/main.047e5b56.chunk.css"
+  },
+  {
+    "revision": "e09d3235d2225b93984f",
+    "url": "/business-card/static/js/2.286eee90.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/business-card/static/js/2.286eee90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b4406dd156f31b0805f",
+    "url": "/business-card/static/js/main.a6a5848c.chunk.js"
+  },
+  {
+    "revision": "ed0fd917ae14715468e2",
+    "url": "/business-card/static/js/runtime-main.b87d5823.js"
+  },
+  {
+    "revision": "c05ece32c14324b2117bc90a8814c70f",
+    "url": "/business-card/static/media/qr-code-2.c05ece32.png"
+  }
+]);

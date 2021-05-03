d4725dbde2ca8f04ea2e50a0d63b6690159e38d9
MatildaBjorken <matilda.bjork@hyperisland.se>
2021-05-03 18:28:13 +0200

    Updates

---
+++ precache-manifest.eca36f2959509294770b653882989673.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6db2004f53a64fb8eb95058af29c2a",
+    "url": "/business-card/index.html"
+  },
+  {
+    "revision": "a586c7f9d7da8beb38d1",
+    "url": "/business-card/static/css/main.061c0865.chunk.css"
+  },
+  {
+    "revision": "dfe82573923361f58662",
+    "url": "/business-card/static/js/2.94cb55b3.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/business-card/static/js/2.94cb55b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a586c7f9d7da8beb38d1",
+    "url": "/business-card/static/js/main.05a0d3f9.chunk.js"
+  },
+  {
+    "revision": "ed0fd917ae14715468e2",
+    "url": "/business-card/static/js/runtime-main.b87d5823.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/business-card/precache-manifest.de5f4197ab41ab2be9c93958e09e1479.js"
+  "/business-card/precache-manifest.eca36f2959509294770b653882989673.js"
 );
 
 self.addEventListener('message', (event) => {
